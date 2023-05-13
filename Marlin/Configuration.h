d7bd3eeb2a8b63370e16140269e79e97bb721c24
Jamie Brindle <jamie.brindle7@gmail.com>
2023-05-13 13:28:03 +0100

    Update to acc

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -736,7 +736,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 136 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 140 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -756,7 +756,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 1000 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 1000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -771,8 +771,8 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          500    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  500    // E acceleration for retracts
+#define DEFAULT_ACCELERATION          750    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  750    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
--- buildroot/share/PlatformIO/scripts/creality.py
+++ buildroot/share/PlatformIO/scripts/creality.py
@@ -2,11 +2,18 @@ import os
 Import("env")
 
 # Relocate firmware from 0x08000000 to 0x08007000
-for define in env['CPPDEFINES']:
-    if define[0] == "VECT_TAB_ADDR":
-        env['CPPDEFINES'].remove(define)
+try:
+    for define in env['CPPDEFINES']:
+        if define[0] == "VECT_TAB_ADDR":
+            env['CPPDEFINES'].remove(define)
+            break
+except Exception as e:
+    print("Error 1: " + str(e))
 
-env['CPPDEFINES'].append(("VECT_TAB_ADDR", "0x08007000"))
+try:
+    env['CPPDEFINES'].append(("VECT_TAB_ADDR", "0x08007000"))
+except Exception as e:
+    print("Error 2: " + str(e))
 
 custom_ld_script = os.path.abspath("buildroot/share/PlatformIO/ldscripts/creality.ld")
 
